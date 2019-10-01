eada18664847692c8ea18e2a2053ea2bc552adf3
gabrielNetto94 <silveranetto94@gmail.com>
2019-10-01 10:29:50 -0300

    finalização exercício internacionalização

--- internacionalizacao/app/src/main/java/com/example/internacionalizacao/MainActivity.java
+++ internacionalizacao/app/src/main/java/com/example/internacionalizacao/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.internacionalizacao;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     String[] items = new String[] {"Brazilian","American","Spanish"};
     Spinner spinner;
+    ImageView fieldImageGender;
 
     @SuppressLint("ResourceType")
     @Override
@@ -19,22 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        /*
         spinner = findViewById(R.id.spinnerNationality);
-        //crio um adaptador com itens já pré-definidos
-        ArrayAdapter<String> adaptador3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-        adaptador3. setDropDownViewResource(android.R.layout.simple_spinner_item);
-        //defino o adaptador do spinner
-        spinner.setAdapter(adaptador3);
-        */
+        fieldImageGender = (ImageView)findViewById(R.id.fieldImage);
 
-        //crio um adaptador com itens já pré-definidos
-        spinner = findViewById(R.id.spinnerNationality);
         ArrayAdapter<CharSequence> adaptador2 = ArrayAdapter.createFromResource(this, R.array.nationality, android.R.layout.simple_spinner_item);
         adaptador2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        //defino o adaptador do spinner
         spinner.setAdapter(adaptador2);
     }
+
+    public void changeImageFemale(View v){
+        fieldImageGender.setImageResource(R.drawable.femaleicon);
+    }
+    public void changeImageMale(View v){
+        fieldImageGender.setImageResource(R.drawable.maleicon);
+    }
+
 
 
 
